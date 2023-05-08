1b0077fc83cf04de184bcf7c268acbf33891419c
ashokit <ashokitschool@gmail.com>
2023-05-08 19:28:29 +0530

    first commit

---
+++ src/main/java/in/ashokit/GreetingRouter.java
@@ -0,0 +1,22 @@
+package in.ashokit;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class GreetingRouter {
+
+	@Bean
+	public RouterFunction<ServerResponse> route(GreetingHandler greeting){
+		return RouterFunctions
+			      .route(GET("/hello")
+			      .and(accept(MediaType.APPLICATION_JSON)), greeting::hello);
+	}
+}
